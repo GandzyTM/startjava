7a6377becd262cf00d772cc65b0808c304f4b616
gandzytm <gandzy1984@gmail.com>
2019-05-31 18:58:29 +0500

    delete doubles

--- Unicode.java
+++
@@ -1,8 +0,0 @@
-public class Unicode {
-    public static void main(String[] args) {
-        for (int i = 9398; i <= 10178; i++) {
-//            char a = (char) (i);
-            System.out.println((char) (i));
-        }
-    }
-}
