49eb6fa9c47a6b91b142326f278e9087f6b695c7
gandzytm <gandzy1984@gmail.com>
2019-05-31 19:18:32 +0500

    Calculator added

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -2,31 +2,31 @@ public class Calculator {
     public static void main(String[] args) {
         int var1 = 3;
         int var2 = 5;
-        char znak = '^';
+        char value = '^';
 
-        if (znak == '+') {
+        if (value == '+') {
             System.out.println(var1 + var2);
         }
-        if (znak == '-') {
+        if (value == '-') {
             System.out.println(var1 - var2);
         }
-        if (znak == '/') {
+        if (value == '/') {
             System.out.println(var1 / var2);
         }
-        if (znak == '*') {
+        if (value == '*') {
             System.out.println(var1 * var2);
         }
-        if (znak == '^') {
+        if (value == '^') {
             int res = 1;
             for (int i = 1; i <= var2; i++) {
                 res = res * var1;
-                System.out.println(res);
             }
+            System.out.println(res);
         }
-        if (znak == '%') {
-            System.out.println(a % b);
+        if (value == '%') {
+            System.out.println(var1 % var2);
         }  else {
-            System.out.println(znak + " error");
+            System.out.println(value + " error");
         }
     }
 }
