50b6186c35190600251c3b8989b5bf6a5f009ae3
gandzytm <gandzy1984@gmail.com>
2019-06-07 20:47:21 +0500

    Variable changed

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,12 +1,20 @@
 public class Variable {
     public static void main(String[] args) {
-        byte byte_var = 5;
-        short short_var = 42;
-        int int_var = -123456;
-        long long_var = 1122334455;
-        float float_var = 3.141592f;
-        double double_var = 3.141592653589d;
-        char symbol = 'Z';
-        boolean logical_var = true;
+        int hdd = 250;
+        byte cpu = 1;
+        short cpu_core = 2;
+        long hdd_kbyte = 262144000;
+        float cpu_hz = 2.30f;
+        double memory = 3.4d;
+        char logical_disk = 'C';
+        boolean b = true;
+        System.out.println("My comp has next params:\nHDD in Kb: " + hdd_kbyte
+        + "\nHDD in Gb: " + hdd
+        + "\nCPU socket: " + cpu
+        + "\nCPU core: " + cpu_core
+        + "\nCPU GHz: " + cpu_hz
+        + "\nRAM Gb: " + memory
+        + "\nLogical disk: " + logical_disk
+        + "\nIs it norm: " + b);
     }
 }
