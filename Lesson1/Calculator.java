9e8a080b5e35d72bc9f2c11ed51560120108a6d3
GandzyTM <gandzy1984@gmail.com>
2019-06-13 11:45:27 +0500

    Update Calculator.java

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,30 +1,30 @@
 public class Calculator {
     public static void main(String[] args) {
-        int var1 = 3;
-        int var2 = 5;
+        int num1 = 3;
+        int num2 = 5;
         char value = '^';
 
         if (value == '+') {
-            System.out.println(var1 + var2);
+            System.out.println(num1 + num2);
         }
         if (value == '-') {
-            System.out.println(var1 - var2);
+            System.out.println(num1 - num2);
         }
         if (value == '/') {
-            System.out.println(var1 / var2);
+            System.out.println(num1 / num2);
         }
         if (value == '*') {
-            System.out.println(var1 * var2);
+            System.out.println(num1 * num2);
         }
         if (value == '^') {
             int res = 1;
-            for (int i = 1; i <= var2; i++) {
-                res *= var1;
+            for (int i = 1; i <= num2; i++) {
+                res *= num1;
             }
             System.out.println(res);
         }
         if (value == '%') {
-            System.out.println(var1 % var2);
+            System.out.println(num1 % num2);
         }  else {
             System.out.println(value + " error");
         }
