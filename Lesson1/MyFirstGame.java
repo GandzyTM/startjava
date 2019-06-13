745632ccb730c9322e1c8ad5f5ec33c5a716b642
GandzyTM <gandzy1984@gmail.com>
2019-06-13 12:11:11 +0500

    Update MyFirstGame.java

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -2,15 +2,24 @@ public class MyFirstGame {
     public static void main(String[] args) {
         System.out.println("Enter a number between 0 and 100: ");
         int computerNum = 47;
-        int userNum = 48;
-        if (computerNum > userNum) {
-            System.out.println("Entered number LESS than the hidden number. Re enter please: ");
+        int userNum = 34;
+        if (userNum < 0) {
+            System.out.println("You entered num < 0");
         }
-        if (computerNum < userNum) {
-            System.out.println("Entered number GREATER than the hidden number. Re enter please: ");
+        else if (userNum > 100) {
+            System.out.println("You entered num > 100");
         }
-        if (computerNum == userNum){
-            System.out.println("You are damn right!!!");
+        else {
+
+            if (computerNum > userNum) {
+                System.out.println("Entered number LESS than the hidden number. Re enter please: ");
+            }
+            else if (computerNum < userNum) {
+                System.out.println("Entered number GREATER than the hidden number. Re enter please: ");
+            }
+            else if (computerNum == userNum){
+                System.out.println("You are damn right!!!");
+            }
         }
     }
 }
