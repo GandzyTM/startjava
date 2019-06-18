e0735850961d33b63f07375ffaec33339bc7ff60
GandzyTM <gandzy1984@gmail.com>
2019-06-18 11:04:31 +0500

    Update MyFirstGame.java

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -2,32 +2,25 @@ public class MyFirstGame {
     public static void main(String[] args) {
         System.out.println("Enter a number between 0 and 100: ");
         int computerNum = 47;
-        int userNum = -5;
+        int userNum = 55;
 
-        if (userNum < 0) {
-            System.out.println("You entered num < 0");
-        } else if (userNum > 100) {
-            System.out.println("You entered num > 100");
-        }
-
-        if (userNum >= 0 || userNum <= 100){
-            while (userNum != computerNum) {
-                if (computerNum > userNum) {
-                    System.out.println("Entered number LESS than the hidden number. Re enter please: ");
-                    while (computerNum > userNum) {
-                        userNum++;
-                    }
-                } else if (computerNum < userNum) {
-                    System.out.println("Entered number GREATER than the hidden number. Re enter please: ");
-                    while (computerNum < userNum) {
-                        userNum--;
-                    }
+        if (userNum >= 0 || userNum <= 100) {
+            if (computerNum > userNum) {
+                System.out.println("Entered number LESS than the hidden number. Re enter please: ");
+                while (computerNum > userNum) {
+                    userNum++;
                 }
-            } // end while
-        } // end if 
-
-        if (computerNum == userNum) {
-            System.out.println("You are damn right!!! The hidden Number is: " + userNum);
+            } else if (computerNum < userNum) {
+                System.out.println("Entered number GREATER than the hidden number. Re enter please: ");
+                while (computerNum < userNum) {
+                    userNum--;
+                }
+            }
+        } else {
+            System.out.println("You entered num > 100 or < 0");
+            return;
         }
+
+        System.out.println("You are damn right!!! The hidden Number is: " + userNum);
     }
 }
