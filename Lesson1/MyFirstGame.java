b543bdb3947d16444bec6c020b857bb0ec6fecd6
GandzyTM <gandzy1984@gmail.com>
2019-06-20 16:17:04 +0500

    Update MyFirstGame.java

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,26 +1,38 @@
 public class MyFirstGame {
     public static void main(String[] args) {
         System.out.println("Enter a number between 0 and 100: ");
-        int computerNum = 47;
-        int userNum = 55;
+        int computerNum = 97;
+        int userNum = 102;
 
-        if (userNum >= 0 || userNum <= 100) {
-            if (computerNum > userNum) {
-                System.out.println("Entered number LESS than the hidden number. Re enter please: ");
-                while (computerNum > userNum) {
-                    userNum++;
-                }
-            } else if (computerNum < userNum) {
-                System.out.println("Entered number GREATER than the hidden number. Re enter please: ");
-                while (computerNum < userNum) {
-                    userNum--;
+        while (computerNum != userNum) {
+            if (userNum < 0 || userNum >100) {
+                System.out.println("You entered number < 0 or > 100");
+                if (userNum < 0) {
+                    while (userNum < 0) {
+                        userNum++;
+                    }
+                } else if (userNum > 100) {
+                    while (userNum > 100) {
+                        userNum--;
+                    }
                 }
             }
-        } else {
-            System.out.println("You entered num > 100 or < 0");
-            return;
+            
+            if (userNum >= 0 && userNum <= 100) {
+                if (computerNum > userNum) {
+                    System.out.println("Entered number LESS than the hidden number. Re enter please: ");
+                    while (computerNum > userNum) {
+                        userNum++;
+                    }
+                } else if (computerNum < userNum) {
+                    System.out.println("Entered number GREATER than the hidden number. Re enter please: ");
+                    while (computerNum < userNum) {
+                        userNum--;
+                    }
+                }
+            }
         }
-
+        
         System.out.println("You are damn right!!! The hidden Number is: " + userNum);
     }
 }
