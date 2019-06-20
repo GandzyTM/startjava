5be85a03724821fe8b3a9a24a367eb8278762312
GandzyTM <gandzy1984@gmail.com>
2019-06-20 16:51:33 +0500

    Update MyFirstGame.java

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -8,31 +8,21 @@ public class MyFirstGame {
             if (userNum < 0 || userNum >100) {
                 System.out.println("You entered number < 0 or > 100");
                 if (userNum < 0) {
-                    while (userNum < 0) {
                         userNum++;
-                    }
                 } else if (userNum > 100) {
-                    while (userNum > 100) {
                         userNum--;
-                    }
                 }
             }
-            
             if (userNum >= 0 && userNum <= 100) {
                 if (computerNum > userNum) {
                     System.out.println("Entered number LESS than the hidden number. Re enter please: ");
-                    while (computerNum > userNum) {
                         userNum++;
-                    }
                 } else if (computerNum < userNum) {
                     System.out.println("Entered number GREATER than the hidden number. Re enter please: ");
-                    while (computerNum < userNum) {
                         userNum--;
-                    }
                 }
             }
         }
-        
         System.out.println("You are damn right!!! The hidden Number is: " + userNum);
     }
 }
