33ae3d96a75942b97da14368a9bab0f33c9bfc6f
GandzyTM <gandzy1984@gmail.com>
2019-06-22 15:52:21 +0500

    Update Wolf.java

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,3 +1,41 @@
 public class Wolf {
+    char sex;
+    String name;
+    float weight;
+    int age;
+    String color;
 
+    void go() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    void howl() {
+
+    }
+
+    void hunt() {
+
+    }
+
+
+}
+
+class WolfTest {
+    public static void main(String[] args) {
+        Wolf wolf = new Wolf();
+
+        wolf.age = 14;
+        wolf.color = "Grey";
+        wolf.name = "Muhtar";
+        wolf.sex = 'M';
+        wolf.weight = 45.7f;
+    }
 }
