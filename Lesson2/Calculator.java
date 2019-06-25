cf9ab6c9dbe36e5c94b641fb55f1c4db15ba57be
gandzytm <gandzy1984@gmail.com>
2019-06-25 21:32:25 +0500

    calc

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -1,45 +1,68 @@
+import java.util.Scanner;
+
 public class Calculator {
     private int num1;
     private int num2;
-    private char operation;
+    private String operation;
 
-    public void setOperation(char operation) {
-        this.operation = operation;
-        System.out.println("Введите знак математической операции: " + operation);
-    }
-
-    public void setNum1(int num1) {
+    public void setNum1() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int num1 = scan.nextInt();
         this.num1 = num1;
-        System.out.println("Введите первое число: " + num1);
     }
 
-    public void setNum2(int num2) {
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum2() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите второе число: ");
+        int num2 = scan.nextInt();
         this.num2 = num2;
-        System.out.println("Введите второе число: " + num2);
     }
 
-    public void getMathOperation() {
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setOperation() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите знак математической операции: ");
+        String operation = scan.nextLine();
+        this.operation = operation;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public int getResult() {
         int mathOperation = 0;
-        switch (operation) {
-            case '+':
-                mathOperation = num1 + num2;
+        switch (getOperation()) {
+            case "+":
+                mathOperation = getNum1() + getNum2();
                 break;
-            case '-':
-                mathOperation = num1 - num2;
+            case "-":
+                mathOperation = getNum1() - getNum2();
                 break;
-            case '/':
-                mathOperation = num1 / num2;
+            case "/":
+                mathOperation = getNum1() / getNum2();
                 break;
-            case '*':
-                mathOperation = num1 * num2;
+            case "*":
+                mathOperation = getNum1() * getNum2();
                 break;
-            case '^':
+            case "%":
+                mathOperation = getNum1() % getNum2();
+                break;
+            case "^":
                 mathOperation = 1;
-                for (int i = 1; i <= num2; i++) {
-                    mathOperation *= num1;
+                for (int i = 1; i <= getNum2(); i++) {
+                    mathOperation *= getNum1();
                 }
                 break;
         }
-        System.out.println(mathOperation);
+        return mathOperation;
     }
 }
--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -1,9 +1,10 @@
 class CalculatorTest {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        calc.setNum1(2);
-        calc.setOperation('^');
-        calc.setNum2(10);
-        calc.getMathOperation();
+        calc.setNum1();
+        calc.setOperation();
+        calc.setNum2();
+        System.out.println(calc.getResult());
     }
 }
+
