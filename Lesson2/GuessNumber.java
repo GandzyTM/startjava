6cc80eb2d09eea7c2c5a5dbee6bb8a2ffe8f79a8
GandzyTM <gandzy1984@gmail.com>
2019-06-28 15:29:45 +0500

    Create GuessNumber.java

---
+++ Lesson2/GuessNumber.java
@@ -0,0 +1,22 @@
+package GuessNumber;
+
+public class Player {
+    private int userNum;
+    private String playerName;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setUserNum(int userNum) {
+        this.userNum = userNum;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getUserNum() {
+        return userNum;
+    }
+}
