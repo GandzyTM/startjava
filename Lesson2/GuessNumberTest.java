c0eece5bba1cf2bfb938e045e31c347fb360f204
GandzyTM <gandzy1984@gmail.com>
2019-06-28 15:32:50 +0500

    Create GuessNumberTest.java

---
+++ Lesson2/GuessNumberTest.java
@@ -0,0 +1,31 @@
+package GuessNumber;
+
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    public static void main(String[] args) {
+
+        String choice = "да";
+        while (choice.equals("да")) {
+            Scanner scan = new Scanner(System.in);
+            Player player1 = new Player("Player1");
+            GuessNumber computer = new GuessNumber();
+            System.out.print("Введите число от 0 до 100: ");
+            player1.setUserNum(scan.nextInt());
+            System.out.println(player1.getPlayerName() + " entered: " + player1.getUserNum());
+            System.out.println(computer.getComputerNum());
+
+
+
+            while (!choice.equals("да") || !choice.equals("нет")) {
+                System.out.print("Хотите продолжить? [да/нет]: ");
+                choice = scan.next();
+//                if (choice.equals("нет")) {
+//                    break;
+//                } else if (choice.equals("да")) {
+//                    break;
+//                }
+            }
+        }
+    }
+}
