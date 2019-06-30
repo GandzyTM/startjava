df055569b065026aa35368c192e9085725c5a1ba
GandzyTM <gandzy1984@gmail.com>
2019-06-30 08:18:02 +0500

    Update GuessNumber.java

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -1,6 +1,5 @@
-package GuessNumber;
-
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumber {
     private int computerNum;
@@ -9,5 +8,16 @@ public class GuessNumber {
         Random random = new Random();
         computerNum = random.nextInt(100);
         return computerNum;
+    }
+
+    public void playGame() {
+        Scanner scan = new Scanner(System.in);
+        Player player1 = new Player("Player1");
+
+        System.out.print("Введите число от 0 до 100: ");
+        player1.setUserNum(scan.nextInt());
+
+        System.out.println(player1.getPlayerName() + " ввел: " + player1.getUserNum());
+        System.out.println(getComputerNum());
     }
 }
