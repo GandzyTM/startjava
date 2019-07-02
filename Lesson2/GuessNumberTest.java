7d71a61898b413fac5318058c45902ef89b6aaff
GandzyTM <gandzy1984@gmail.com>
2019-07-02 16:21:21 +0500

    Update GuessNumberTest.java

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
-        Random random = new Random();
 
-        System.out.print("Enter name for First player: ");
-        Player player1 = new Player(scan.next(), 0);
+        String choice;
+        do {
+            System.out.println("First player enter your name: ");
+            Player player1 = new Player();
+            System.out.println("Second player enter your name: ");
+            Player player2 = new Player();
+            System.out.println("Today play: " + player1.getName() + " " + player2.getName());
 
-        System.out.print("Enter name for Second player: ");
-        Player player2 = new Player(scan.next(), 0);
+            System.out.println("Computer got a number. What number do you think?");
 
-        System.out.println("Today play: " + player1.getPlayerName() + " " + player2.getPlayerName());
-
-        Player computer = new Player("Computer", random.nextInt(100));
-        System.out.println("Computer got a number. What number do you think?");
-
-        System.out.println(player1.getPlayerName() + " please enter your number: ");
-        player1.setUserNum(scan.nextInt());
-        GuessNumber playGame = new GuessNumber(player1.getUserNum(), computer.getUserNum());
-        playGame.playGame();
-    }
+            System.out.print(player1.getName() + " please enter your number: ");
+            player1.setNumber();
+            GuessNumber playGame = new GuessNumber(player1.getNumber());
+            playGame.playGame();
+            System.out.print("Хотите продолжить? [да/нет]: ");
+            choice = scan.next();
+        } while (!choice.equals("да") && !choice.equals("нет"));
+    } // end of method main
 }
