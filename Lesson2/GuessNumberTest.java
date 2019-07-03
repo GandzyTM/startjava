ec182b11eb95f25590574b93f529baf291734136
GandzyTM <gandzy1984@gmail.com>
2019-07-03 15:18:10 +0500

    Update GuessNumberTest.java

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -1,12 +1,11 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
         String choice;
         do {
+            Scanner scan = new Scanner(System.in);
+
             System.out.println("First player enter your name: ");
             Player player1 = new Player();
             System.out.println("Second player enter your name: ");
@@ -15,12 +14,22 @@ public class GuessNumberTest {
 
             System.out.println("Computer got a number. What number do you think?");
 
-            System.out.print(player1.getName() + " please enter your number: ");
-            player1.setNumber();
-            GuessNumber playGame = new GuessNumber(player1.getNumber());
-            playGame.playGame();
-            System.out.print("Хотите продолжить? [да/нет]: ");
-            choice = scan.next();
+            boolean result = false;
+            while (!result) {
+                GuessNumber playGame = new GuessNumber();
+                System.out.print(player1.getName() + " please enter your number: ");
+                player1.setNumber();
+                playGame.playGame(player1.getNumber());
+
+                System.out.print(player2.getName() + " please enter your number: ");
+                player2.setNumber();
+                playGame.playGame(player2.getNumber());
+
+            }
+
+        System.out.print("Хотите продолжить? [да/нет]: ");
+        choice = scan.next();
         } while (!choice.equals("да") && !choice.equals("нет"));
     } // end of method main
 }
+
