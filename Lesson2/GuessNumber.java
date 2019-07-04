780c4af77e5b202f7846b8d2365e106d1f41ff1a
GandzyTM <gandzy1984@gmail.com>
2019-07-04 09:00:59 +0500

    Update GuessNumber.java

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -4,26 +4,21 @@ public class GuessNumber {
     Random random = new Random();
     private int computerNum = random.nextInt(100);;
 
-    public boolean playGame(int userNum) {
-        boolean result;
+    public void playGame(int userNum) {
         while(computerNum != userNum) {
             if (userNum < 0 || userNum > 100) {
                 System.out.println("You entered number < 0 or > 100");
-                return result = false;
             }
 
             if (userNum >= 0 && userNum <= 100) {
                 if (computerNum > userNum) {
                     System.out.println("Entered number LESS than the hidden number. Re enter please: ");
-                    return result = false;
                 } else if (computerNum < userNum) {
                     System.out.println("Entered number GREATER than the hidden number. Re enter please: ");
-                    return result = false;
                 }
             }
         } // end of main while
         System.out.println("You are right!!! it is a " + computerNum);
-        return result = true;
     } // end void playGame
 }
 
