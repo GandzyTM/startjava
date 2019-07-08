a01f2ef1a265339f39bd818aa8763d357e010923
GandzyTM <gandzy1984@gmail.com>
2019-07-08 16:49:49 +0500

    Update GuessNumberTest.java

--- Lesson2/GuessNumberTest.java
+++ Lesson2/GuessNumberTest.java
@@ -1,42 +1,26 @@
 import java.util.Scanner;
 
 public class GuessNumberTest {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String choice = "да";
-        while (choice.equals("да")) {
-            System.out.print("First player enter your name: ");
-            Player player1 = new Player(scan.next());
-            
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String choice = "да";
+		while (choice.equals("да")) {
+			System.out.print("First player enter your name: ");
+			Player player1 = new Player(scan.next());
+
 			System.out.print("Second player enter your name: ");
-            Player player2 = new Player(scan.next());
-            
+			Player player2 = new Player(scan.next());
+
 			System.out.println("Today play: " + player1.getName() + " " + player2.getName());
-            System.out.println("Computer got a number. What number do you think?");
+			System.out.println("Computer got a number. What number do you think?");
 
-            int count = 1;
-            GuessNumber playGame = new GuessNumber();
-            while (count != 0) {
-                if (count == 1) {
-                    System.out.print(player1.getName() + " please enter your number: ");
-                    player1.setNumber();
-                    playGame.playGame(player1.getNumber());
-                    count = playGame.getCount();
-                }
+			GuessNumber playGame = new GuessNumber(player1.getName(), player2.getName());
+			playGame.playGame();
 
-                if (count == 1) {
-                    System.out.print(player2.getName() + " please enter your number: ");
-                    player2.setNumber();
-                    playGame.playGame(player2.getNumber());
-                    count = playGame.getCount();
-                }
-            }
-
-            do {
-                System.out.print("Хотите продолжить? [да/нет]: ");
-                choice = scan.next();
-            } while (!choice.equals("да") && !choice.equals("нет"));
-        } // end of main while
-    } // end of method main
+			do {
+				System.out.print("Хотите продолжить? [да/нет]: ");
+				choice = scan.next();
+			} while (!choice.equals("да") && !choice.equals("нет"));
+		} // end of main while
+	} // end of method main
 }
-
