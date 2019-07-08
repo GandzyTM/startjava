4946e68eeabd973745c78027116bc7f470df252d
GandzyTM <gandzy1984@gmail.com>
2019-07-08 16:48:39 +0500

    Update Player.java

--- Lesson2/Player.java
+++ Lesson2/Player.java
@@ -1,24 +1,11 @@
-import java.util.Scanner;
-
 public class Player {
-    private int number;
-    private String name;
-    private boolean result;
+	private String name;
 
-    public Player(String name) {
-        this.name = name;
-    }
+	public Player(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
-
-    public void setNumber() {
-        Scanner scan = new Scanner(System.in);
-        this.number = scan.nextInt();
-    }
-
-    public int getNumber() {
-        return number;
-    }
+	public String getName() {
+		return name;
+	}
 }
