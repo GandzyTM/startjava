bf8d72878bc89fac23968ad87d6a8338bb487c00
GandzyTM <gandzy1984@gmail.com>
2019-07-09 09:52:53 +0500

    Update GuessNumber.java

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -7,65 +7,58 @@ public class GuessNumber {
 	private int count;
 	private String player1;
 	private String player2;
-	private int userNum;
 
-    public GuessNumber(String player1, String player2) {
-        this.player1 = player1;
-        this.player2 = player2;
-    }
+	public GuessNumber(String player1, String player2) {
+		this.player1 = player1;
+		this.player2 = player2;
+	}
 
-    public int getCount() {
+	public int getCount() {
 		return count;
 	}
 
 	public void setCount(int count) {
 		this.count = count;
 	}
+	
+	public void playGame() {
+		Scanner scan = new Scanner(System.in);
+		int count = 1;
+		while (count != 0) {
+			if (count == 1) {
+				Player player = new Player(player1);
+				System.out.print(player1 + " please enter your number: ");
+				player.setNumber(scan.nextInt());
+				checkNum(player.getNumber());
+				count = getCount();
+			}
 
-    public int getUserNum() {
-        return userNum;
-    }
-
-    public void setUserNum(int userNum) {
-        this.userNum = userNum;
-    }
-
-    public void playGame() {
-        Scanner scan = new Scanner(System.in);
-        int count = 1;
-        while (count != 0) {
-            if (count == 1) {
-                System.out.print(player1 + " please enter your number: ");
-                setUserNum(scan.nextInt());
-                checkNum(getUserNum());
-                count = getCount();
-            }
-
-            if (count == 1) {
-                System.out.print(player2 + " please enter your number: ");
-                setUserNum(scan.nextInt());
-                checkNum(getUserNum());
-                count = getCount();
-            }
-        }
+			if (count == 1) {
+				Player player = new Player(player2);
+				System.out.print(player2 + " please enter your number: ");
+				player.setNumber(scan.nextInt());
+				checkNum(player.getNumber());
+				count = getCount();
+			}
+		}
 	} // end void playGame
 
-    private void checkNum(int userNum) {
-        if (userNum < 0 || userNum > 100) {
-            System.out.println("You entered number < 0 or > 100");
-        }
+	private void checkNum(int userNum) {
+		if (userNum < 0 || userNum > 100) {
+			System.out.println("You entered number < 0 or > 100");
+		}
 
-        if (userNum >= 0 && userNum <= 100) {
-            if (computerNum > userNum) {
-                System.out.println("Entered number LESS than the hidden number. Next player ");
-                setCount(1);
-            } else if (computerNum < userNum) {
-                System.out.println("Entered number GREATER than the hidden number. Next player ");
-                setCount(1);
-            } else if (computerNum == userNum) {
-                System.out.println("You are right!!! it is a " + computerNum);
-                setCount(0);
-            }
-        }
-    }
+		if (userNum >= 0 && userNum <= 100) {
+			if (computerNum > userNum) {
+				System.out.println("Entered number LESS than the hidden number. Next player ");
+				setCount(1);
+			} else if (computerNum < userNum) {
+				System.out.println("Entered number GREATER than the hidden number. Next player ");
+				setCount(1);
+			} else if (computerNum == userNum) {
+				System.out.println("You are right!!! it is a " + computerNum);
+				setCount(0);
+			}
+		}
+	}
 }
