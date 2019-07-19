40815b1da703f9d8e4d0bda4e15d414b2e2fa45d
GandzyTM <gandzy1984@gmail.com>
2019-07-19 13:17:10 +0500

    Update Calculator.java

--- src/com/startjava/lesson_4/array/calculator/Calculator.java
+++ src/com/startjava/lesson_4/array/calculator/Calculator.java
@@ -1,23 +1,48 @@
 package com.startjava.lesson_4.array.calculator;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 class Calculator {
     private int num1;
     private int num2;
     private String operation;
 
-    void setNum1(int num1) {
+    private void setNum1(int num1) {
         this.num1 = num1;
     }
 
-    void setNum2(int num2) {
+    int getNum1() {
+        return num1;
+    }
+
+    private void setNum2(int num2) {
         this.num2 = num2;
     }
 
-    void setOperation(String operation) {
+    int getNum2() {
+        return num2;
+    }
+
+    private void setOperation(String operation) {
         this.operation = operation;
     }
 
-    int calculate() {
+    String getOperation() {
+        return operation;
+    }
+
+    void setArray() {
+        Scanner scan = new Scanner(System.in);
+        String[] expression = new String[3];
+        Arrays.setAll(expression, i -> scan.next());
+
+        setNum1(Integer.parseInt(expression[0]));
+        setNum2(Integer.parseInt(expression[2]));
+        setOperation(expression[1]);
+    }
+
+    int calculate(int num1, String operation, int num2) {
         switch (operation) {
             case "+":
                 return num1 + num2;
@@ -37,3 +62,4 @@ class Calculator {
         return 0;
     }
 }
+
