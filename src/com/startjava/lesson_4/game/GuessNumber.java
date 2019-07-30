b8d4a43188ce96674e0e713daacc565e16fd0c57
GandzyTM <gandzy1984@gmail.com>
2019-07-30 13:03:39 +0500

    Update GuessNumber.java

--- src/com/startjava/lesson_4/game/GuessNumber.java
+++ src/com/startjava/lesson_4/game/GuessNumber.java
@@ -8,7 +8,6 @@ class GuessNumber {
     private Scanner scan = new Scanner(System.in);
     private Player player1;
     private Player player2;
-    private int choiceNumber;
 
     GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -33,25 +32,26 @@ class GuessNumber {
 
     private void inputNumber(Player player) {
         do {
-            System.out.print(player.getName() + " please enter your number: ");
+            System.out.print(player.getName() + " введите число: ");
         } while (!player.setNumber(scan.nextInt()));
     }
 
     private boolean compareNumbers(Player player) {
         if (computerNum > player.getNumber()) {
             System.out.println(player.getName() + " вы ввели число МЕНЬШЕ загаданного. Следующий игрок.");
-//            player.setChoice(choiceNumber++);
+            player.encreaseChoice();
             player.setEnteredNumbers(player.getNumber());
         } else if (computerNum < player.getNumber()) {
             System.out.println(player.getName() + " вы ввели число БОЛЬШЕ загаданного. Следующий игрок.");
-//            player.setChoice(choiceNumber++);
+            player.encreaseChoice();
             player.setEnteredNumbers(player.getNumber());
         } else if (computerNum == player.getNumber()) {
-//            player.setChoice(choiceNumber++);
+            player.encreaseChoice();
             player.setEnteredNumbers(player.getNumber());
-            System.out.println(player.getName() + "Игрок " + player.getName()
+            System.out.println("Игрок " + player.getName()
                     + " угадал число " + computerNum
-                    + " с " + player.getTries() + " попытки.");
+                    + " с " + player.getChoice() + " попытки.");
+            System.out.println("Введенные игроком числа: " + player.getTries());
             return true;
         }
         return false;
